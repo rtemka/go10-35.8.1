22eaafa8a3764ad5f56d780755f076c57c579ab2
Artem Rybakov <gizmo3000@yandex.ru>
2022-06-07 11:28:42 +0300

    add client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,1 +1,43 @@
 package client
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"time"
+)
+
+const timeout = time.Second * 5
+
+func Dial(ctx context.Context, proto, socket string) error {
+	childCtx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	var d net.Dialer
+	conn, err := d.DialContext(childCtx, proto, socket)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = conn.Close() }()
+
+	r := bufio.NewReader(conn)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+			p, err := r.ReadBytes('\n')
+			if err != nil {
+				if err != io.EOF {
+					return err
+				}
+				return nil
+			}
+
+			fmt.Printf("received: %s\n", string(p))
+		}
+	}
+}
