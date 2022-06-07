88c76d66546f5a33c7f007113495eabad7b0b3ee
Artem Rybakov <gizmo3000@yandex.ru>
2022-06-07 11:29:44 +0300

    add main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"os"
+	"proverbserver/pkg/client"
+	"proverbserver/pkg/server"
+	"time"
+)
 
+const proto = "tcp"
+
+func main() {
+	socket := os.Getenv("PROVERB_SERVER_SOCKET")
+	if socket == "" {
+		log.Fatal("$PROVERB_SERVER_SOCKET environment variable must be set")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
+	defer cancel()
+
+	go func() {
+		err := server.Listen(ctx, proto, socket)
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	err := client.Dial(ctx, proto, socket)
+	if err != nil {
+		log.Println(err)
+	}
 }
